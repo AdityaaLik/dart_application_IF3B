74f8c8bf5b4e17938232a6f65ff67a62685ccbed
Adityaalikusuma <adityaalikusuma_2226250022@mhs.mdp.ac.id>
2023-10-03 12:59:55 +0700

    Masukan Tugas 1.2

--- bin/dart_application_if3b.dart
+++ bin/dart_application_if3b.dart
@@ -1,6 +1,17 @@
 import 'package:dart_application_if3b/dart_application_if3b.dart'
     as dart_application_if3b;
 
-void main(List<String> arguments) {
-  print('Hello everyonegit');
-}
+
+void main() {
+  int panjang = 5;
+  int lebar = 3;
+
+  PersegiPanjang persegi = PersegiPanjang(panjang, lebar);
+
+  int luas = persegi.hitungLuas();
+  int keliling = persegi.hitungKeliling();
+
+  print('Panjang Persegi Panjang : $panjang Lebar Persegi Panjang : $lebar');
+  print('Luas Persegi Panjang: $luas');
+  print('Keliling Persegi Panjang: $keliling');
+} 
